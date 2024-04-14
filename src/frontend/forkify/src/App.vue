82c1a2d65e42ecf8f8c580d59f022a5ef0c84eb2
Yousaf Anwar <yousafanwar74@gmail.com>
2024-04-14 07:48:24 +0400

    first commit

---
+++ src/frontend/forkify/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+      <Nav />
+      <Recipe />
+    </div>
+</template>
+
+
+<script>
+// import Recipe from '../components/Recipe';
+import Recipe from './components/Recipe.vue';
+import Nav from './components/NavBar.vue';
+
+export default{
+  components:{
+    Recipe,
+    Nav
+  }
+}
+</script>
+
+<style>
+  body{
+    background-color: #b2ebf2;
+    /* #757575 grey darken-1 */
+
+  }
+</style>
---
+++ src/frontend/forkify/src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+            <nav>
+          <div class="nav-wrapper">
+            <a href="#" class="brand-logo">
+            <img v-bind:src="logo" alt="" class="circle responsive-img" style="width: 120px; height: 60px;"> <!-- notice the "circle" class -->
+          </a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a href="sass.html">Sass</a></li>
+              <li><a href="badges.html">Components</a></li>
+              <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+          </div>
+      </nav>
+</template>
+
+<script>
+  import logo from '../assets/img/logo.png';
+
+export default{
+  data(){
+    return{
+      logo: logo
+    }
+  }
+}
+</script>
+
+<style>
+    nav{
+      background-color: #e0f7fa
+    }
+</style>
